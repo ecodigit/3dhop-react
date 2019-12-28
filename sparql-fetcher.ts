33a596c39cb3d9fb79aa1920e5d69d70d4caa8f8
Marco Puccini <puccini.marco@gmail.com>
2019-12-28 15:44:15 +0100

    query small updates

--- sparql-fetcher.ts
+++ sparql-fetcher.ts
@@ -1,6 +1,8 @@
 import {SparqlEndpointFetcher} from "fetch-sparql-endpoint";
+var endpoint = 'http://150.146.207.67/sparql/ds';
+var query = 'SELECT * WHERE { ?s ?p ?o } LIMIT 100'
 const myFetcher = new SparqlEndpointFetcher();
-let a = myFetcher.fetchBindings('http://150.146.207.67/sparql/ds', 'SELECT * WHERE { ?s ?p ?o } LIMIT 100');
+let a = myFetcher.fetchBindings(endpoint, query);
 a.then(s => {
   s.on("data", (bindings) => console.log(bindings));
 });
