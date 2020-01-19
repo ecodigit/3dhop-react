596baa0af26ddb95bacb7936de8979b2c0ad4291
marco.puccini <marco.puccini@enea.it>
2020-01-20 00:07:54 +0100

    Dockerfile fixed

--- src/sparql-fetcher.ts
+++
@@ -1,8 +0,0 @@
-import {SparqlEndpointFetcher} from "fetch-sparql-endpoint";
-var endpoint = 'http://150.146.207.67/sparql/ds';
-var query = 'SELECT * WHERE { ?s ?p ?o } LIMIT 100'
-const myFetcher = new SparqlEndpointFetcher();
-let a = myFetcher.fetchBindings(endpoint, query);
-a.then(s => {
-  s.on("data", (bindings) => console.log(bindings));
-});
