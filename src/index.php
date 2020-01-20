79f50fb83bc1e4987c5db28bfda7738952b8303a
marco.puccini <marco.puccini@enea.it>
2020-01-20 09:55:57 +0100

    index.php small adds

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 	$html ="<h1>risultati ricerca</h1>";
         $html ="<h2>Modelli 3D</h2>";
+        $html ="<hr>";
 	$html.='<div><a href="3dv.php?model=models/trono_corsini.nxz&unitmeas=cm">trono</a></div>';
 	$html.='<div><a href="3dv.php?model=models/busto_bernini.nxz&unitmeas=dm">busto</a></div>';
 	$html.='<div><a href="3dv.php?unitmeas=m&model=models/Porta_Latina_e.nxz&submodel_1=models/Porta_Latina_i_sx.nxz&submodel_2=models/Porta_Latina_interno.nxz">mura</a></div>';
