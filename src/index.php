2fb4ccc3a8fc7cbf5704d111f04fc5714db293ee
marco.puccini <marco.puccini@enea.it>
2020-01-21 18:28:54 +0100

    index.php small fixes

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 	$html ="<h1>Risultati Ricerca</h1>";
-        $html ="<h3>Modelli 3D</h3>";
+	$html.="<hr>";
+    $html.="<h3>Modelli 3D</h3>";
 	$html.='<div><a href="assembler.php?url=https://w3id.org/ecodigit/object/enea/trono_corsini">Trono Corsini</a></div>';
 	$html.='<div><a href="assembler.php?url=https://w3id.org/ecodigit/object/saccone/porta_latina">Mura Aureliane</a></div>';
 
