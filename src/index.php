bf18ae5af0c2363680f78bc0e4fbd7d882de4ba6
mpuccini <puccini.marco@gmail.com>
2020-01-22 17:38:25 +0100

    small fixes in index

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 	$html ="<h1>Risultati Ricerca</h1>";
 	$html.="<hr>";
     $html.="<h3>Modelli 3D</h3>";
-	$html.='<div><a href="assembler_old.php?url=https://w3id.org/ecodigit/object/enea/trono_corsini">Trono Corsini</a></div>';
+	$html.='<div><a href="assembler.php?url=https://w3id.org/ecodigit/object/enea/trono_corsini">Trono Corsini</a></div>';
 	$html.='<div><a href="assembler.php?url=https://w3id.org/ecodigit/object/saccone/porta_latina">Mura Aureliane</a></div>';
 
 echo $html;
